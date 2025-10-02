ed8d38bbe59af4c6683f61bef8d77168395ec5ce
Azizbek <azizbekphd@gmail.com>
2025-10-02 16:48:26 +0500

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import * as cover from './utils/tile-cover'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Impacts } from './routes/Impacts/Impacts'
@@ -7,9 +6,6 @@ function App() {
   return <Routes>
     <Route path="/" element={ <p>1</p> } />
     <Route path="/sac25/impact/" element={ <Impacts /> } />
-    <Route path="/sac25/amogus/" element={
-      <p>{ JSON.stringify(cover.tiles({ type: 'Point', coordinates: [27.987695, 86.925267]}, { max_zoom: 14, min_zoom: 7 })) }</p>
-    } />
   </Routes>
 }
 
