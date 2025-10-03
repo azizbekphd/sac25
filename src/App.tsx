eb1bb8ccd2da3f815d1857fcaa32157116baa0bd
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2025-10-03 09:18:34 +0500

    feat: update default route to render Orrery component instead of placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Impacts } from './routes/Impacts/Impacts'
+import { Orrery } from './routes/Orrery/Orrery'
 
 function App() {
   return <Routes>
     <Route path="sac25">
-      <Route index element={<p>1</p>} />
+      <Route index element={ <Orrery /> } />
       <Route path="impact" element={ <Impacts /> } />
     </Route>
   </Routes>
