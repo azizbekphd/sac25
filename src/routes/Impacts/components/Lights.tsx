91e693b30b245376dcd08a9e9855e3d37f09d84c
Azizbek <azizbekphd@gmail.com>
2025-10-04 06:55:12 +0500

    add: sky color

--- src/routes/Impacts/components/Lights.tsx
+++ src/routes/Impacts/components/Lights.tsx
@@ -1,4 +1,13 @@
+import { useThree } from "@react-three/fiber"
+import { useEffect } from "react"
+import { Color } from "three"
+
 export const Lights = () => {
+  const { scene } = useThree()
+  useEffect(() => {
+    scene.background = new Color('#87CEEB')
+  }, [scene])
+
   return (
     <>
       <ambientLight intensity={0} />
