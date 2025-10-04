f22ae54f292a35a1d3d2141afdf70bdd4290d334
Azizbek <azizbekphd@gmail.com>
2025-10-04 15:48:57 +0500

    add: asteroid burning effect

--- src/routes/Impacts/components/Asteroid.tsx
+++ src/routes/Impacts/components/Asteroid.tsx
@@ -11,7 +11,7 @@ export const Asteroid = () => {
 
   useFrame((_, delta) => {
     if (!asteroidRef.current || !asteroidRef.current.parent) return
-    const velocity = 1
+    const velocity = 0.01
     speed.current += delta * velocity
     asteroidRef.current.position.y -= speed.current
     if (asteroidRef.current.position.y <= 0 && performance.now() >= 3000) {
@@ -21,14 +21,43 @@ export const Asteroid = () => {
   })
 
   return (
-    <>
+    <group
+      ref={asteroidRef}
+      position={new THREE.Vector3(0, 3, 0)}
+    >
       <UnwrappedModel
-        ref={asteroidRef}
         source={'/sac25/models/asteroid.glb'}
-        position={new THREE.Vector3(0, 100, 0)}
+        position={new THREE.Vector3(0, 0, 0)}
         scale={0.0003}
         color={'grey'}
       />
-    </>
+      <mesh
+        position={new THREE.Vector3(0, 0, 0)}
+        rotation={[-Math.PI / 2, 0, 0]}
+      >
+        <circleGeometry args={[3, 100]} />
+        <shaderMaterial
+          vertexShader={`
+            varying float d;
+
+            void main() {
+              d = distance(uv, vec2(0.5, 0.5)) * 3.14159265359;
+              gl_Position = projectionMatrix * modelViewMatrix * vec4(position + vec3(0.0, 0.0, -0.2 + d), 1.0);
+            }
+            `}
+          fragmentShader={`
+            varying float d;
+
+            void main() {
+              gl_FragColor = vec4(0.96, 0.58, 0.17, smoothstep(0.2, 0.0, d));
+            }
+            `}
+          transparent
+          side={THREE.DoubleSide}
+          blending={THREE.AdditiveBlending}
+          depthWrite={false}
+        />
+      </mesh>
+    </group>
   )
 }
