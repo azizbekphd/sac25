2307153177499c9c76c195efdb7fb3ca31f73380
Lazizjon-web-dev <azizjonovlazizjon64@gmail.com>
2025-10-05 05:53:54 +0500

    Add Slider component with range inputs for speed and impact angle, and corresponding styles

--- src/routes/Orrery/components/SideMenu/index.tsx
+++ src/routes/Orrery/components/SideMenu/index.tsx
@@ -1,18 +1,14 @@
 import { FocusContext } from "../../contexts";
-import { FiltersMenu, BodiesTable } from "../index";
+import { FiltersMenu, BodiesTable, Slider } from "../index";
 import SelectedBody from "../SelectedBody";
 import "./index.css";
 import { useContext, useEffect, useRef, useState } from "react";
-
-const [speedMin, speedMax] = [1, 250];
-const [angleMin, angleMax] = [5, 90];
 
 const SideMenu: React.FC = () => {
   const [show, setShow] = useState<boolean>(true);
   const { selected } = useContext(FocusContext);
   const sideMenuRef = useRef<HTMLDivElement>(null);
-  const [speed, setSpeed] = useState<number>(38);
-  const [angle, setAngle] = useState<number>(45);
+
 
   useEffect(() => {
     if (!selected.objectId) {
@@ -33,6 +29,7 @@ const SideMenu: React.FC = () => {
               <h2>Menu</h2>
 
               <FiltersMenu />
+              <Slider />s
               <BodiesTable />
             </>
           )}
@@ -40,40 +37,6 @@ const SideMenu: React.FC = () => {
         <button className="side-menu-toggler" onClick={() => setShow(!show)}>
           {show ? "Close" : "Menu"}
         </button>
-      </div>
-      <div className="slider-input-wrapper">
-        <div className="slider-title-wrapper">
-          <div className="slider-title-name">Speed:</div>
-          <div>{speed},000 mph</div>
-        </div>
-        <input
-          type="range"
-          min={speedMin}
-          max={speedMax}
-          value={speed}
-          step={1}
-          onChange={(e) => {
-            setSpeed(parseInt(e.target.value));
-          }}
-          name=""
-          id=""
-        />
-        <div className="slider-title-wrapper">
-          <div className="slider-title-name">Impact angle:</div>
-          <div>{angle}°</div>
-        </div>
-        <input
-          type="range"
-          min={angleMin}
-          max={angleMax}
-          value={angle}
-          step={1}
-          onChange={(e) => {
-            setAngle(parseInt(e.target.value));
-          }}
-          name=""
-          id=""
-        />
       </div>
     </>
   );
